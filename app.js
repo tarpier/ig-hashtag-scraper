1076efcf47bbb08ef1421182f70fe7bfba8a0e53
Ove Doll <ove@ovedoll.de>
2017-03-02 23:28:47 +0100

    writing to file, filename is the same as account name

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var Nightmare = require('nightmare');
-var vo = require('vo');
+var Nightmare = require('nightmare')
+var vo = require('vo')
+var fs = require('fs')
+
+var a = [];
+var user = 'derblitz'
 
 vo(function* () {
     var nightmare = Nightmare({
@@ -7,24 +11,25 @@ vo(function* () {
     });
 
     let imgSources = yield nightmare
-        .goto('https://www.instagram.com/explore/tags/festival/')
+        .goto(`https://www.instagram.com/${user}/`)
         .wait('._8imhp')
-        //.click('._8imhp')
+        .click('._8imhp')
         .evaluate(function () {
             return Array.from(document.querySelectorAll('._8mlbc')).map(element => element.href);
         })
 
     for (src in imgSources) {
         yield nightmare.goto(imgSources[src])
-            // here should be some kind of check if the links has worked
+            // here should be some kind of check if the links have worked
             .wait('h1') 
             .evaluate(function () {
+                //is there a show all link? ._l086v._ifrvy
                 let input = Array.from(document.querySelectorAll('._nk46a span')).map(element => element.textContent);
-                //find Hashtags here
                 return input;
             })
             .then(result => {
-                console.log(result)
+                //console.log(result)
+                a.push(result);    
             });
     }
 
@@ -34,6 +39,11 @@ vo(function* () {
     if (err) {
         console.log('caught', err);
     } else {
-        console.log('done');
+        console.log('done', a);
+        fs.writeFile(`data/${user}.json`, JSON.stringify(a), (err) => {
+            if(err) { 
+                console.log(err);
+            }
+        });
     }
 });
